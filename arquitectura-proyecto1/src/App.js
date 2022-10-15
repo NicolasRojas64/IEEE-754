a0c12f81272f7e96569bd222afb06826822e9f52
NicolasRojas64 <nicolas.rojas04@uptc.edu.co>
2022-10-15 10:19:41 -0500

    Inicio presición simple

---
+++ arquitectura-proyecto1/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import SimplePresicion from './SimplePrecision';
+function App() {
+  return (
+    <div className="App">
+      <SimplePresicion />
+    </div>
+  );
+}
+
+export default App;
---
+++ arquitectura-proyecto1/src/SimplePrecision.js
@@ -0,0 +1,82 @@
+function SimplePresicion(){
+    let number = 951.35;
+    //let number = 71.3125;
+    let [integerPart, decimalPart] = number.toString().split(".")
+    let binaryNumber = number.toString(2);
+    let [binaryIntegerPart, binaryDecimalPart] = binaryNumber.split(".");
+
+    function denormalize(){
+        let corrimiento;
+        if(number > 0){
+            let a = binaryNumber.replace(".","");
+            corrimiento = a.slice(0,1) + "." + a.slice(2)
+        }
+        return corrimiento;
+    }
+
+    function calculateExponent(){
+        let exp;
+        if(number > 0){
+            let index = binaryNumber.indexOf(".");
+            exp = "127 + "+ (index-1) + " = " + (127 + (index - 1)) + " (10) --> "+ (127 + (index - 1)).toString(2) + " (2)";
+        }
+        return exp;
+    }
+
+    function exponent(){
+        let exp;
+        if(number > 0){
+            let index = binaryNumber.indexOf(".");
+            exp = (127 + (index - 1)).toString(2);
+        }
+        return exp;
+    }
+
+   function calculateSign(){
+        let s;
+        if(number >= 0){
+            s = 0;
+        }else{
+            s = 1;
+        }
+        return s;
+   }
+
+   function mantissa(){
+        let denormalized = denormalize();
+        let mantissa = denormalized.slice(2);
+        console.log(23 - (mantissa.length))
+        if(mantissa.length <= 23){
+            for(let i; i <= (23 - (mantissa.length)); i++){
+                mantissa.concat("0");
+            }
+        }
+        return mantissa;
+   }
+
+    return(
+        <div>
+            
+            <h2>Número a convertir: {number}</h2>
+            <h2>Parte entera en binario: {integerPart + " (10) --> " + binaryIntegerPart + " (2)"}</h2>
+
+            <h2>Parte decimal en binario: {decimalPart + " (10) --> " + binaryDecimalPart.substring(0,30) + " (2)"}</h2>
+
+            <h2>Unión de parte entera(2) y parte decimal(2): {binaryIntegerPart + "." + binaryDecimalPart.substring(0,30)}</h2>
+
+            <h2>Corrimiento hacia el uno más cercano: {denormalize()}</h2>
+
+            <h2>Exponente en binario: {calculateExponent()}</h2>
+
+            <h2>Conversión a presición simple (32 bits): </h2>
+
+            <h3>Signo: {calculateSign()}</h3>
+            <h3>Exponente: {exponent()}</h3>
+            <h3>Mantisa: {mantissa()}</h3>
+
+            <h2>Conversión de presición simple a hexadecimal: </h2>
+        </div>
+    );
+}
+
+export default SimplePresicion;
